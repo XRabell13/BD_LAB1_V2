20a129aaf31087f067814a408825281e3157f642
XRabell13 <XRabell13@rambler.ru>
2021-09-10 08:22:54 +0300

    New files

--- app/src/main/java/com/example/bd_lab1_v2/MainActivity.java
+++ app/src/main/java/com/example/bd_lab1_v2/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.bd_lab1_v2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.example.bd_lab1_v2.text.TextFunction;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        displayText();
+
+        for (int count = 0; count < 10; count++) {
+            Log.d("MainActivity", "counter = " + count);
+        }
+    }
+
+    private void displayText() {
+        TextFunction tf = new TextFunction();
+        TextView nt = findViewById(R.id.newtest);
+        nt.setText(tf.getValue());
     }
 }
---
+++ app/src/main/java/com/example/bd_lab1_v2/text/TextFunction.java
@@ -0,0 +1,8 @@
+package com.example.bd_lab1_v2.text;
+
+public class TextFunction {
+    public  String getValue()
+    {
+        return "Hello from first project";
+    }
+}
